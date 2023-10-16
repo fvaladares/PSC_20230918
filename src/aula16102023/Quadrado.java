b1ddb6a7a4f7d984a9a485cde29f198b3a351c73
Fabricio Valadares <fabricio.valadares@gmail.com>
2023-10-16 20:17:37 -0300

    Solução do problema de impressão do quadrado de *

--- src/aula16102023/Quadrado.java
+++ src/aula16102023/Quadrado.java
@@ -2,7 +2,15 @@ package aula16102023;
 
 public class Quadrado {
     public void imprimirQuadrado(int tamanhoQuadrado) {
+        // Controla as linhas
+        // (executado de forma mais "lenta"
+        // Uma iteração para cada "tamanho quadrado"
+        // do for interno.
         for(int i = 0; i < tamanhoQuadrado; i++) {
+            // Controla as colunas
+            // Executado de forma mais "rápida"
+            // "tamanhoQuadrado" iterações para cada
+            // iteração do for externo.
             for (int j = 0; j < tamanhoQuadrado; j++) {
                 System.out.print("* ");
             }
